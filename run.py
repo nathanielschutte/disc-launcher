d73f78f2e7f3615b96fbe4440eaa9218bdbf324a
nathanielschutte <nks222@lehigh.edu>
2022-02-03 01:36:35 -0500

    run and update code

--- run.py
+++ run.py
@@ -1,8 +1,40 @@
 
 # Run bot script
 
+import os, sys
+
 def main():
-    pass
+    print('hello')
+
+verbose = '-v' in sys.argv
+
+required_dirs = [
+    'bot',
+    'config'
+]
+
+def log(*msgs):
+    if verbose:
+        print(' '.join(msgs))
+
+def check_env() -> None:
+    log('Checking bot environment...')
+    try:
+        for dir in required_dirs:
+            assert os.path.isdir(dir), f'directory not found: {dir}'
+    except AssertionError as e:
+        log(f'Environment error: {e}')
+        exit(1)
+
+    log('Environment OK')
+
+def main() -> int:
+
+    check_env()
+
+    from bot.launcher import Runner
+    bot = Runner()
+    return bot.run()
 
 if __name__ == '__main__':
-    main()
+    log('Exited with code', main())
