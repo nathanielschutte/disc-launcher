f910db632b276fdb255ff59d8a2d72550240cb07
nathanielschutte <nks222@lehigh.edu>
2022-02-07 18:46:24 -0500

    begin implementation for game

--- games/guessing_game/guessing_game.py
+++ games/guessing_game/guessing_game.py
@@ -1,3 +1,17 @@
 
 # Simple turn-based game implementation
 
+# Load DiscGame from absolute path for now
+# This should be done through pip eventually
+import sys
+sys.path.append('C:/Users/Nate/Documents/projects/repos/disc-launcher/bot')
+from bot.game import DiscGame
+
+# Implement the basic DiscGame object
+class GuessingGame(DiscGame):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+    
+    def start(self):
+        print(f'Hello from {self.__class__}')
